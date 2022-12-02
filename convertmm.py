ec01b06ca1b7957c31a766e89715a14ff445a118
John Millsap <jmillsapengineer@gmail.com>
2022-12-02 13:49:38 -0600

    better comments

--- convertmm.py
+++ convertmm.py
@@ -1,12 +1,16 @@
+# simple utility to convert .asc grids from millimeters to inches.
 import os
 import sys
 from pathlib import Path
 
+# initialize files list
 files = []
 path = Path.cwd()
+# add all .asc files in the current working directory to files list
 for i in os.listdir(path):
     if i.endswith(".asc"):
         files.append(i)
+# if no .asc files are found, exit
 if len(files) == 0:
     sys.exit("No .asc files found in this directory")
 
@@ -14,13 +18,20 @@ for f in files:
     reader = open(f, "r")
     writer = open("new.asc", "a")
     for i, line in enumerate(reader):
+        # .asc grids have five lines of header material. This if statement ensures they stay the same.
         if i <= 5:
             writer.write(line)
+        # this if statement performs a conversion on each grid pixel (mm to in)
         elif i > 5:
             word_list = str.split(line)
+            # -9999 is used as the default nodata value in .asc grids - make sure they stay the same.
+            # .asc grids are literally just text files with the same number of words (pixels) in each line.
+            # Because of this each "word" in the line has to be converted to float.
             word_list = [-9999 if float(w) == -9999 else float(w) / 25.4 for w in word_list]
             writer.write(" ".join([str(w) for w in word_list]) + "\n")
     reader.close()
     writer.close()
+    # delete the old file
     os.remove(f)
+    # rename the new file with the same name (in effect, replace the old file with the new, converted file)
     os.rename("new.asc", f)
--- convertnegative.py
+++ convertnegative.py
@@ -1,25 +1,38 @@
+# simple utility to remove small negative values in .asc grids
+# some XMRG files from NOAA seem to have small negative values - if you choose, this utility converts them
+# to nodata values.
 import sys
 import os
 from pathlib import Path
 
+# initialize files list
 files = []
 path = sys.argv[1]
 
+# add .asc files to files list
 for i in os.listdir(path):
     if i.endswith(".asc"):
         files.append(i)
 
+if len(files) == 0:
+    sys.exit('no .asc files found in this directory')
+
 for f in files:
     reader = open(f, "r")
     writer = open("new.asc", "a")
+    # .asc grid files have 6 lines of header material. This if statement ensures they get re-written.
     for i, line in enumerate(reader):
         if i <= 5:
             writer.write(line)
         elif i > 5:
             word_list = str.split(line)
+            # the default nodata value in .asc grids is -9999; this if statement converts all negatives
+            # to nodata
             word_list = [-9999 if float(w) < 0 else w for w in word_list]
             writer.write(" ".join([str(w) for w in word_list]) + "\n")
     reader.close()
     writer.close()
+    # remove old file
     os.remove(f)
+    # rename new file to same name as old file - essentially replacing it
     os.rename("new.asc", f)
